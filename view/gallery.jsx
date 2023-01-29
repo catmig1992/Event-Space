862407d4f6f24c67d7cc6f67e51412c8414bad15
Your Name <you@example.com>
2023-01-29 12:11:55 -0800

    Change codes for home and gallery

--- view/gallery.jsx
+++ view/gallery.jsx
@@ -1,29 +1,207 @@
 const React = require('react')
-const Def = require('./default.jsx.js')
+const Def = require('./default.jsx')
+
 
 function gallery () {
     return (
       <Def>
 <main>
-  
-    <h1>SPARKLY EVENT PLANNER</h1>
-    <div>
-      <img src="" alt="" height="100px" width="100px"/> ///Still need to choose image///
-      <div>
-        <h2>About us</h2>
-        <p>Planning a PARTY is our business! We are here for your every need to get the party started.</p>
-        <p>To get started, click "Build Your Own Event" and fill out the form. If you have questions.</p>
+<section class="">
+ 
+  <section class="">
+    <div class="row">
+        
+      <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="image/birthday.jpeg"
+            class="w-100"
+          />
+          <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal1">
+            <div class="mask"></div>
+          </a>
+        </div>
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="image/kid_birthday.jpeg"
+            class="w-100"
+          />
+          <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal1">
+            <div class="mask"></div>
+          </a>
+        </div>
+        
+      </div>
+
+
+
+
+
+      <div class="col-lg-4 mb-4 mb-lg-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="/image/wedding.jpeg"
+            class="w-100"
+          />
+          <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal2">
+            <div class="mask"></div>
+          </a>
+        </div>
+                <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="image/party_people2.jpeg"
+            class="w-100"
+          />
+          <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal2">
+            <div class="mask"></div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 mb-4 mb-lg-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="image/party_people.jpeg"
+            class="w-100"
+          />
+          <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal3">
+            <div class="mask" ></div>
+          </a>
+        </div>
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="image/newyear_celebration.jpeg"
+            class="w-100"
+          />
+          <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal3">
+            <div class="mask" ></div>
+          </a>
+        </div>
       </div>
     </div>
-    <a href="">//Still need a route for the button// 
-        <button className="btn-primary">Build Your Own Event</button>
-    </a>
+  </section>
+
+
+
+  <section class="">
+ 
+    <div
+      class="modal fade"
+      id="exampleModal1"
+      tabindex="-1"
+      aria-labelledby="exampleModal1Label"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="ratio ratio-16x9">
+            <iframe
+              src="https://www.youtube.com/embed/A3PDXmYoF5U"
+              title="YouTube video"
+              allowfullscreen
+            ></iframe>
+          </div>
+
+          <div class="text-center py-3">
+            <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
+            
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+    <div
+      class="modal fade"
+      id="exampleModal2"
+      tabindex="-1"
+      aria-labelledby="exampleModal2Label"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="ratio ratio-16x9">
+            <iframe
+              src="https://www.youtube.com/embed/wTcNtgA6gHs"
+              title="YouTube video"
+              allowfullscreen
+            ></iframe>
+          </div>
+
+          <div class="text-center py-3">
+            <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
+            
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="modal fade"
+      id="exampleModal3"
+      tabindex="-1"
+      aria-labelledby="exampleModal3Label"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="ratio ratio-16x9">
+            <iframe
+              src="https://www.youtube.com/embed/vlDzYIIOYmM"
+              title="YouTube video"
+              allowfullscreen
+            ></iframe>
+          </div>
+
+          <div class="text-center py-3">
+            <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
+            
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</section>
+
+    
 </main>
 
 
       </Def>
     )
   }
-  
+
+
+
+
+
+
+
+
+
 
 module.exports = gallery
+
+  
+
+
